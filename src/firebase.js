9d7c234500714f9d25e3dfe3250be11770783e38
Cristian <crysscal@gmail.com>
2026-07-26 20:15:41 +0200

    aggiunto login con autenticazione Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyB3wjavGLZlgwt9Z91tqu-s8tI7AAezsbI",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
+export const auth = getAuth(app)
